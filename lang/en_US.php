55d94b568ca2bf23f8ecd0d1796a3cf79326224a
sebastiand <dubbel@dubbel.local>
2012-07-09 14:15:34 +0200

    EXTENSION: Added dynamic Google Map (JavaScript API v3), completely configurable via CMS + additional contact detail fields
    - Added additional fields for configuration of Google Map JavaScript
    map (height and width can now be set in CMS as well)
    - Added new function Addressable->DynamicAddressMap() that renders a
    dynamic google map
    - Added new template "DynamicAddressMap.ss"
    - Changed template "AddressMap.ss" to use the map config fields (width,
    height, zoom)
    - Added translations for "en_US" and "de_DE"

---
+++ lang/en_US.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * English (US) language pack
+ * @subpackage i18n
+ */
+
+global $lang;
+
+
+/**
+ * Class Addressable
+ */
+$lang['en_US']['Addressable']['TABTITLE'] = 'Address Map';
+$lang['en_US']['Addressable']['COMPANYHEADER'] = 'Company Name';
+$lang['en_US']['Addressable']['COMPANYNAME'] = 'Company Name';
+$lang['en_US']['Addressable']['ADDRESSHEADER'] = 'Address';
+$lang['en_US']['Addressable']['ADDRESS'] = 'Street address';
+$lang['en_US']['Addressable']['SUBURB'] = 'Suburb';
+$lang['en_US']['Addressable']['STATE'] = 'State';
+$lang['en_US']['Addressable']['POSTCODE'] = 'Postcode';
+$lang['en_US']['Addressable']['COUNTRY'] = 'Country';
+$lang['en_US']['Addressable']['CONTACTDATA'] = 'Contact Details';
+$lang['en_US']['Addressable']['TELEPHONE'] = 'Telephone';
+$lang['en_US']['Addressable']['FAX'] = 'Fax';
+$lang['en_US']['Addressable']['EMAIL'] = 'Email';
+$lang['en_US']['Addressable']['MAPSETUPHEADER'] = 'Map Setup';
+$lang['en_US']['Addressable']['MAPWIDTH'] = 'Map Width';
+$lang['en_US']['Addressable']['MAPHEIGHT'] = 'Map height';
+$lang['en_US']['Addressable']['DYNAMICMAPSETUPHEADER'] = 'Dynamic Map Setup (Javascript Google Map)';
+$lang['en_US']['Addressable']['MAPZOOM'] = 'Zoom Factor of Map (1 = worldview - 21 = street view)';
+$lang['en_US']['Addressable']['MAPPANCONTROL'] = 'Show Pan Control';
+$lang['en_US']['Addressable']['MAPZOOMCONTROL'] = 'Show Zoom Control';
+$lang['en_US']['Addressable']['MAPZOOMSTYLE'] = 'Zoom Control Styling';
+$lang['en_US']['Addressable']['MAPTYPECONTROL'] = 'Show Map Type Control';
+$lang['en_US']['Addressable']['MAPTYPESTYLE'] = 'Map Type Control Styling';
+$lang['en_US']['Addressable']['MAPTYPEID'] = 'Map Type';
+$lang['en_US']['Addressable']['MAPSCALECONTROL'] = 'Show Scale Control';
+$lang['en_US']['Addressable']['MAPSTREETVIEWCONTROL'] = 'Show StreetView Control';
+$lang['en_US']['Addressable']['MAPOVERVIEWMAPCONTROL'] = 'Show Overview Map Control';
+
+$lang['en_US']['DynamicAddressMap.ss']['CALCULATEROUTE'] = 'Get routing information';
+
+
